6e099195b1f6f7e401089080295536886ce9bc24
Efecan Havus <Ehavus@gmail.com>
2022-11-21 15:25:59 -0500

    Made graph into abstract class

--- src/main/java/statsVisualiser/graph/Graph.java
+++ src/main/java/statsVisualiser/graph/Graph.java
@@ -2,8 +2,12 @@ package statsVisualiser.graph;
 
 import javax.swing.JPanel;
 
-public class Graph {
+public abstract class Graph {
 	public JPanel panel;
 	public Graph(String country, int yearStart, int yearEnd, String analysis) {}
 	public Graph() {}
+	
+	
+	public abstract boolean wrongAnalysisType(String country, int yearStart, int yearEnd, String analysis);
+	public abstract boolean isEmpty(String country, int yearStart, int yearEnd, String analysis);
 }
