11bc26d692687af49a77af5a2b4064675e076d4c
Efecan Havus <Ehavus@gmail.com>
2022-11-21 13:28:20 -0500

    added main in analysis

--- src/main/java/analysis/AnalysisMain.java
+++ src/main/java/analysis/AnalysisMain.java
@@ -1,21 +1,32 @@
 package analysis;
 
-import java.util.ArrayList;
-import java.util.List;
+	import java.util.ArrayList;
+	import java.util.List;
 
-import fetchers.Data;
-import fetchers.DataSet;
-
+	import fetchers.Data;
+	import fetchers.DataSet;
 public class AnalysisMain {
 
-public static void main(String[] args) {
-		
-		List<DataSet> datasetList = new ArrayList<DataSet>(); 
-		AnnualPercentChange method = new AnnualPercentChange();
-		
-		datasetList.add(Data.fetchData("CAN",2021 , 1999, "EN.ATM.CO2E.PC"));
-		 
-		 DataSet ds = method.getAnalyzedData(datasetList);
+	
+	
+	
+
+
+
+	public static void main(String[] args) {
+			
+			List<DataSet> datasetList = new ArrayList<DataSet>(); 
+			AnnualPercentChange method = new AnnualPercentChange();
+			
+			datasetList.add(Data.fetchData("CAN",2021 , 1999, "EN.ATM.CO2E.PC"));
+			 
+			 DataSet ds = method.getAnalyzedData(datasetList);
+		}
+
 	}
 
-}
+	
+	
+	
+	
+
